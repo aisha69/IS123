74c91063aa6aefcc1366a9571f2bd470ff0b1be0
Aishamaharjan <91249950+aisha69@users.noreply.github.com>
2024-09-27 19:26:32 +0545

    ans1

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ alert("Hello class!");
 
 alert("The value of 4/3+1/3+1/3 is:  "+(4/3+1/3+1/3));
 alert("The value of 1/3+1/3+4/3 is:  "+(1/3+1/3+4/3));
+alert(
+    function sumMultiplesOf3And5(limit) {
+        let total = 0;
+        
+        for (let i = 0; i < limit; i++) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                total = total+i;
+            }
+        }
+        
+        return total;
+    }
+    
+    )
